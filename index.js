a7af02f5cbfed136226c2943870cc36dbb939730
Hal Helms <hal.helms@gmail.com>
2019-01-21 17:22:12 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Intro To Functions Lab
+
+/*
+1. Create a function, joinStrings, that accepts 2 strings and joins them using the + operator. (this is known as string concatenation), returning the result.
+*/ 
+
+/* 
+2. Call joinStrings 3 separate times -- the first time sending 1 argument, the second time sending 2 strings, and the third time sending 3 strings. What happens?
+*/
+
+/*
+3. Fix this code so that it returns "This is a messed-up...function, yo"
+*/ 
+
+function fixMe (
+  let str: This is a messed-up...;
+  return: {str};
+  str + str + "function, yo";
+);
+
+/*
+4. 
+*/
